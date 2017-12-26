4e2380c2bd4e096b9fdf7b39a16cab8df8615a42
Maxim Pisarenko <m.pisarenko@magdv.com>
2017-12-26 17:30:20 +0700

    Initial commit

---
+++ TOSearchBarKit/TOSearchBarKit.h
@@ -0,0 +1,20 @@
+//
+//  TOSearchBarKit.h
+//  TOSearchBarKit
+//
+//  Created by Maxim Pisarenko on 26/12/2017.
+//  Copyright © 2017 Okrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TOSearchBarKit/TOSearchBar.h>
+
+//! Project version number for TOSearchBarKit.
+FOUNDATION_EXPORT double TOSearchBarKitVersionNumber;
+
+//! Project version string for TOSearchBarKit.
+FOUNDATION_EXPORT const unsigned char TOSearchBarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TOSearchBarKit/PublicHeader.h>
+
+
